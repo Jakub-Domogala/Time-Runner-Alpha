1d5e95807729c1c0dc8b1ab3370ac338c720c672
MichalPokrzywa <michpok467@student.polsl.pl>
2024-03-23 20:04:00 +0100

    Added TimerModyfiyer

---
+++ Assets/Scripts/TimerMultiplayerObject.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerMultiplayerObject : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    [SerializeField] ObjectOptions options;
+    [SerializeField] float setMultiplayer = 3;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (options)
+        {
+            case ObjectOptions.SetValue:
+                GameMaster.Instance.SetTimeMultiplayer(setMultiplayer);
+                break;
+            case ObjectOptions.Decrease:
+                GameMaster.Instance.DecreaseMultiplayer();
+                break;
+            case ObjectOptions.Increase:
+                GameMaster.Instance.IncreaseMultiplayer();
+                break;
+        }
+    }
+}
+
+enum ObjectOptions
+{
+    SetValue = 1,
+    Decrease = 2,
+    Increase = 3
+
+}
