a978d488d606b5805a738b40bb55b2c4a744d09d
MichalPokrzywa <michpok467@student.polsl.pl>
2024-03-23 19:02:19 +0100

    time master

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
+    [SerializeField] float timeMultiplayer;
+    [SerializeField] float timer;
+    [SerializeField] float timeIncrease;
 
+    [SerializeField] TMP_Text text;
+    [SerializeField] TMP_Text text2;
     // Start is called before the first frame update
     void Start()
     {
-      
+        timer = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer = Time.time;
+        timeMultiplayer += (Time.deltaTime * timeIncrease);
+        text.text = timeMultiplayer.ToString();
+        text2.text = (timer*timeMultiplayer).ToString();
     }
 }
